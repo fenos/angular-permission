b711aaa31c1d2cd42a6107a88e6d66b8d33f0499
Can Tecim <ctecim@mynextmatch.com>
2016-05-02 18:30:27 +0300

    Update angular-permission.js

--- dist/angular-permission.js
+++ dist/angular-permission.js
@@ -44,7 +44,7 @@
             // if (!$rootScope.$broadcast('$stateChangeStart', toState, toParams, fromState, fromParams).defaultPrevented) {
               $rootScope.$broadcast('$stateChangePermissionAccepted', toState, toParams);
 
-              $state.go(toState.name, toParams, angular.merge({}, transitionOptions, {notify: false, location:'replace'}).then(function() {
+              $state.go(toState.name, toParams, angular.merge({}, transitionOptions, {notify: false, location:'replace'})).then(function() {
                 $rootScope
                   .$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
               });
